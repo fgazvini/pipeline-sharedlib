98ac9b79e012ad26c54a9ad5fad8915ebf1cd0b8
Farzin Assadi <fgazvini@gmail.com>
2018-10-10 23:26:05 +0000

    debug getCredential.groovy

--- vars/getCredential.groovy
+++ vars/getCredential.groovy
@@ -2,8 +2,8 @@
 
 def call(String name = 'human') {
     withCredentials([usernamePassword(credentialsId: 'sonar-secrets', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-       sh 'echo  $PASSWORD'
-       sh 'echo $USERNAME > ./credentials.txt'
-       sh 'echo "username is: $USERNAME" >> ./credentials.txt'
+       sh '''echo  ${PASSWORD}'''
+       sh '''echo ${USERNAME} > ./credentials.txt'''
+       sh '''echo "username is: ${USERNAME}" >> ./credentials.txt'''
    }
 }
