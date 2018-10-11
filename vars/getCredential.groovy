352ba4ca81256723801a1c213034544af0e87363
fgazvini <fgazvini@users.noreply.github.com>
2018-10-11 23:04:48 +0200

    Update getCredential.groovy
    removed paramteter from call

--- vars/getCredential.groovy
+++ vars/getCredential.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+def call() {
     withCredentials([usernamePassword(credentialsId: 'sonar-secrets', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
        sh '''echo "Password is: ${PASSWORD}"'''
        sh '''echo ${USERNAME} > ./credentials.txt'''
