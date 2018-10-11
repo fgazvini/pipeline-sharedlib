fabb78d836f584b93d71d11c50be4790c9db4231
fgazvini <fgazvini@users.noreply.github.com>
2018-10-12 00:07:00 +0200

    Update getCredential.groovy

--- vars/getCredential.groovy
+++ vars/getCredential.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    stage('credential') {
+    node {
         withCredentials([usernamePassword(credentialsId: 'sonar-secrets', passwordVariable: 'password', usernameVariable: 'username')]) {
             echo "Hello!!!"
             sh "echo ${username} ${password}"
